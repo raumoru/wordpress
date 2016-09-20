1def1dc12b02cd38ee9930a5db5abba3d8249739
raumoru <e16007@std.it-college.ac.jp>
2016-09-20 20:27:59 +0900

    ちゃぷたー4のせくしょん3まで

--- single.php
+++ single.php
@@ -2,10 +2,10 @@
 
     <div class="contentsWrap">
         <div class="mainContents">
-            <?php 
+            <?php
             if ( have_posts() ) :
                 while ( have_posts() ) : the_post();
-            ?>              
+            ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class('entry'); ?>>
                 <h1 class="type-A">新着情報</h1>
                 <h2 class="title type-B"><span><?php the_title(); ?></span></h2>
@@ -20,16 +20,16 @@
                 <section class="content">
                 <?php the_content(); ?>
                 </section><!-- /.content -->
-
+                <?php comments_template(); ?>
                 <nav class="postNavi">
                     <span class="prev"><?php previous_post_link('%link'); ?></span>
                     <span class="next"><?php next_post_link('%link'); ?></span>
                 </nav>
             </article><!-- /.entry -->
-            <?php 
+            <?php
                 endwhile;
             endif;
-            ?>              
+            ?>
         </div><!-- /.mainContents -->
 
         <aside class="subContents">
